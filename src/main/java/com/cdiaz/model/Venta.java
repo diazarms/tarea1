39299f7088739cd25b6be3ed943c88fdf8f8a36b
diazarms <diazarms@gmail.com>
2020-10-05 12:29:54 -0300

    Test2

--- src/main/java/com/cdiaz/model/Venta.java
+++ src/main/java/com/cdiaz/model/Venta.java
@@ -1,5 +1,78 @@
 package com.cdiaz.model;
 
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.ForeignKey;
+import javax.validation.constraints.PositiveOrZero;
+
+@Entity
+@Table(name = "venta")
 public class Venta {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id_venta;
+	
+	@PositiveOrZero(message = "El valor debe ser mayor o igual a cero")
+	@Column(name = "costo", nullable = false)
+	private Integer costo;
+	
+	private LocalDateTime fecha_venta;
+	
+	@ManyToOne
+	@JoinColumn(name = "id_vendedor", nullable = false, foreignKey = @ForeignKey(name = "FK_venta_vendedor"))
+	private Supervisor supervisor;
+	
+	@ManyToOne
+	@JoinColumn(name = "id_cliente", nullable = false, foreignKey = @ForeignKey(name = "FK_venta_cliente"))
+	private Cliente cliente;
+
+	public Integer getId_venta() {
+		return id_venta;
+	}
+
+	public void setId_venta(Integer id_venta) {
+		this.id_venta = id_venta;
+	}
+
+	public Integer getCosto() {
+		return costo;
+	}
+
+	public void setCosto(Integer costo) {
+		this.costo = costo;
+	}
+
+	public LocalDateTime getFecha_venta() {
+		return fecha_venta;
+	}
+
+	public void setFecha_venta(LocalDateTime fecha_venta) {
+		this.fecha_venta = fecha_venta;
+	}
+
+	public Supervisor getSupervisor() {
+		return supervisor;
+	}
+
+	public void setSupervisor(Supervisor supervisor) {
+		this.supervisor = supervisor;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
 
 }
